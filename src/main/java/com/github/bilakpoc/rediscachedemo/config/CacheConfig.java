f610323801f53e3189cecaf5a7ef41996eb9fdc5
ozkanpakdil <ozkan.pakdil@gmail.com>
2020-06-16 20:50:47 +0200

    change config for json

--- src/main/java/com/github/bilakpoc/rediscachedemo/config/CacheConfig.java
+++ src/main/java/com/github/bilakpoc/rediscachedemo/config/CacheConfig.java
@@ -28,37 +28,21 @@ public class CacheConfig {
     this.objectMapper = objectMapper;
   }
 
-//    @Bean
-//    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
-//        return builder -> builder.cacheDefaults(defaultCacheConfig(Duration.ofMinutes(2)));
-//    }
-//
-//    private RedisCacheConfiguration defaultCacheConfig(Duration ttl) {
-//        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator
-//                .builder()
-//                .allowIfBaseType(ModelImport.class)
-//                .allowIfSubType(ModelImport.class)
-//                .build();
-//        objectMapper.activateDefaultTyping(ptv);
-//        var genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-//
-//        var jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-//        ObjectMapper om = new ObjectMapper();
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
-//
-//
-////        var jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(ModelImport.class);
-////        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-////        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-//
-//        return RedisCacheConfiguration.defaultCacheConfig()
-//                .disableCachingNullValues()
-//                .entryTtl(ttl)
-//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-////                        genericJackson2JsonRedisSerializer
-//                        jackson2JsonRedisSerializer
-//                ));
-//    }
+   @Bean
+   public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+       return builder -> builder.cacheDefaults(defaultCacheConfig(Duration.ofMinutes(2)));
+   }
+
+   private RedisCacheConfiguration defaultCacheConfig(Duration ttl) {
+       var jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(ModelImport.class);
+       objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+       jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+       return RedisCacheConfiguration.defaultCacheConfig()
+               .disableCachingNullValues()
+               .entryTtl(ttl)
+               .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                       jackson2JsonRedisSerializer
+               ));
+   }
 }
